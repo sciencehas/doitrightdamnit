81a69bf54394ef7b3f1da6d50228f18183947a18
Tanya M <130961406+sciencehas@users.noreply.github.com>
2023-07-10 05:46:08 -0400

    Update forms.py

--- project_root/app/forms.py
+++ project_root/app/forms.py
@@ -1,6 +1,4 @@
-```python
 from django import forms
 
 class UploadForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label='Upload files')
-```
